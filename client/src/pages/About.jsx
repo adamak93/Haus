81b09b04cf8980e5c5c027c5f519d8836cc01a96
Adam <mail@adamkhafagy.com>
2023-12-29 03:45:12 -0500

    Finished Home and About Page, changed Title of the app, preparing app for deployment, final commit.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+    <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+    About GroverHaus
+    </h1>
+    <p className='mb-4 text-slate-700'>Introducing GroverHaus, your premier destination for all things real estate. At GroverHaus, we understand that finding the perfect place to call home is a journey, and we're here to make that journey seamless and enjoyable. Our user-friendly website empowers you to explore a vast array of properties available for buying, selling, or renting with just a few clicks. Much like Zillow, GroverHaus provides an intuitive search function allowing you to filter your options based on essential criteria such as the number of bedrooms, bathrooms, and the availability of parking. Whether you're in search of a cozy apartment, a spacious family home, or a chic downtown loft, GroverHaus has the tools to turn your dream home into a reality.</p>
+    <p className='mb-4 text-slate-700'>What sets GroverHaus apart is our commitment to providing a comprehensive and tailored experience. Looking for a furnished place or interested in exclusive discounts? GroverHaus has got you covered. Our platform is designed to cater to your specific needs, ensuring that you not only find the right property but also enjoy the perks that matter most to you. With GroverHaus, your journey to finding the perfect home is not just efficient—it's an exciting adventure.</p>
+    <p className='mb-4 text-slate-700'>Join the GroverHaus community and unlock a world of possibilities in real estate. Whether you're a first-time buyer, a seasoned investor, or someone looking to rent a temporary space, GroverHaus is the one-stop destination where your housing aspirations meet endless opportunities. Explore, discover, and make your next move with confidence at GroverHaus.</p>
+    </div>
   )
 }
